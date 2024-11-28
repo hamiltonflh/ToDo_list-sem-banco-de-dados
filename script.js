a30857c2e5d1a21b97c15d2fdd92a0c28d843537
hamiltonflh <161047380+hamiltonflh@users.noreply.github.com>
2024-11-27 21:02:06 -0300

    func de concluir as tarefas

--- script.js
+++ script.js
@@ -4,20 +4,11 @@ var btn = document.querySelector('#btnAdd');
 var divTarefas = document.querySelector('.tarefas');
 var zeroTarefas = document.querySelector('#titulo_sem_tarefa');
 
-//Adicionar Tarefa
-
-function adicionarTarefa(){
-    zeroTarefas.style.display = 'none';
-    var geradorTarefa => () {
-        geradorCard(inputText.value);
-    }
-    inputText.value = '';
-}
-
-btn.addEventListener('click', adicionarTarefa);
 
 
-function gereadorCard(inputText){
+
+//Funções para as Tarefas...
+function geradorCard(inputText){
     var tarefa = document.createElement('div');
     tarefa.className = 'tarefa';
 
@@ -25,11 +16,11 @@ function gereadorCard(inputText){
     btnConcluir.id = 'conclude_';
     var iconeCheck = document.createElement('i');
     iconeCheck.className = 'material-symbols-outlined';
-    iconeCheck.innerText = 'check';
+    iconeCheck.innerHTML = 'check';
     btnConcluir.appendChild(iconeCheck);
 
     var text = document.createElement('p');
-    text.innerText = inputText;
+    text.innerHTML= inputText;
 
     var divOptions = document.createElement('div');
     divOptions.className = 'options';
@@ -38,27 +29,52 @@ function gereadorCard(inputText){
     btnEdit.id = 'edit_';
     var iconeEdit = document.createElement('i');
     iconeEdit.className = 'material-symbols-outlined';
-    iconeEdit.innerText = 'edit';
+    iconeEdit.innerHTML = 'edit';
     btnEdit.appendChild(iconeEdit);
 
     var btnDelete = document.createElement('button');
     btnDelete.className = 'delete_';
     var iconeDelete = document.createElement('i');
     iconeDelete.className = 'material-symbols-outlined';
-    iconeCheck.innerText = 'delete';
+    iconeDelete.innerHTML = 'delete';
     btnDelete.appendChild(iconeDelete);
 
     divOptions.appendChild(btnEdit);
     divOptions.appendChild(btnDelete);
 
-
-
-    
     tarefa.appendChild(btnConcluir);
     tarefa.appendChild(text);
     tarefa.appendChild(divOptions);
 
+    btnConcluir.addEventListener('click', function(event) {
+        if(tarefa.className == "tarefa"){
+            tarefa.className = "tarefa_done";
+        } else{
+            tarefa.className = "tarefa";
+        }
+    });
 
-    divTarefas.appendChild(tarefa);
+
+    return tarefa;
+}
+
+function adicionarTarefa(){
+    if(inputText.value !== ''){
+        zeroTarefas.style.display = 'none';
+        divTarefas.appendChild(geradorCard(inputText.value));
+        inputText.value = '';
+        inputText.placeholder = 'Adicione uma tarefa';
+    } else{
+        inputText.placeholder = 'Digite uma tarefa!!';
+    }
 
 }
+
+btn.addEventListener('click', adicionarTarefa);
+
+inputText.addEventListener('keypress', function(event) {
+    if(event.key === 'Enter'){
+        adicionarTarefa();
+    }
+})
+
